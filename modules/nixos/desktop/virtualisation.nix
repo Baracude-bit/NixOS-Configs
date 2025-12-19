ed0105a91e9dc998b2fa155e922499c18a4d9b9c
Baracude-bit <toladragos@tutamail.com>
2025-12-19 16:55:19 +0200

    Add files via upload

---
+++ modules/nixos/desktop/virtualisation.nix
@@ -0,0 +1,29 @@
+{ pkgs, ... }:
+{
+  programs.virt-manager.enable = true;
+
+  virtualisation = {
+    docker.enable = true;
+
+    libvirtd = {
+      enable = true;
+      onBoot = "ignore";
+      qemu = {
+        swtpm.enable = true;
+        vhostUserPackages = with pkgs; [ virtiofsd ];
+      };
+    };
+  };
+
+  environment.systemPackages = with pkgs; [ docker-compose ];
+
+  users.users.emi.extraGroups = [
+    "libvirtd"
+    "docker"
+  ];
+
+  boot.kernelParams = [
+    "intel_iommu=on"
+    "iommu=pt"
+  ];
+}
