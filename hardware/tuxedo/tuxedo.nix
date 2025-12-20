b06aa73e725a073c7c7b29439f8321d93d01f6ca
ChickenIQ <emibenzar@gmail.com>
2025-12-20 20:32:45 +0200

    add tuxedo-rs

--- hardware/tuxedo/tuxedo.nix
+++ hardware/tuxedo/tuxedo.nix
@@ -3,4 +3,9 @@
   imports = with inputs; [
     nixos-hardware.nixosModules.tuxedo-infinitybook-pro14-gen7
   ];
+
+  hardware.tuxedo-rs = {
+    enable = true;
+    tailor-gui.enable = true;
+  };
 }
