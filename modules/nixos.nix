3a33f6defdc8aca1de262533fa6a36c5c3039007
Baracude-bit <xCnhmibqNPeMnsK@proton.me>
2025-12-21 19:56:09 +0200

    Little changes

--- modules/nixos.nix
+++ modules/nixos.nix
@@ -278,17 +278,6 @@
   # ================================================================
   # NIX CONFIGURATION
   # ================================================================
-  system.autoUpgrade = {
-    enable = true;
-    flake = inputs.self.outPath;
-    flags = [
-      "--update-input"
-      "nixpkgs"
-      "-L" # print build logs
-    ];
-    dates = "weekly";
-    randomizedDelaySec = "45min";
-  };
 
   nix = {
     channel.enable = false;
